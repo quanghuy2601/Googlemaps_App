a24fa2f44f8d5ae6377288dbef868dcb09ec3294
quanghuy2601 <quanghuy7927@gmail.com>
2023-07-17 13:02:57 +0700

    first commit

---
+++ android/app/src/main/kotlin/android/demo/ggmap/MainActivity.kt
@@ -0,0 +1,6 @@
+package android.demo.ggmap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,3 @@
+class Secrets {
+  static const API_KEY = 'AIzaSyBxJ-LcTSZGekbQU60qiUXkf_TdoAjfk9c';
+}
